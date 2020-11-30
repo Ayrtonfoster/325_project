b9f6450a1f98366509293f8fda24df6a3ab1b8c7
Ayrton <ayrtonfoster@gmail.com>
2020-11-30 18:21:52 -0500

    fix 2

--- CI-Python/qa327/frontend.py
+++ CI-Python/qa327/frontend.py
@@ -13,7 +13,7 @@ The html templates are stored in the 'templates' folder.
 """
 
 # Reusable REGEX
-ticket_name_reg = re.compile("^\S[a-zA-Z0-9_ ]{1,60}\S$")
+ticket_name_reg = re.compile(r'^\S[a-zA-Z0-9_ ]{1,60}\S$')
 """The name of the ticket has to be alphanumeric-only, and space allowed only 
     if it is not the first or the last character. The name of the ticket is 
     no longer than 60 characters"""

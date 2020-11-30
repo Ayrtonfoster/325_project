9f6593929ecf1a0f417039caecd88d7fdd293e9a
Ayrton <ayrtonfoster@gmail.com>
2020-11-30 17:45:12 -0500

    fixing

--- CI-Python/qa327/frontend.py
+++ CI-Python/qa327/frontend.py
@@ -78,8 +78,7 @@ def check_user_format(email, password, name=None, password2=None):
     error_message = None
 
     # Email must conform to RFC 5322
-    # CANNOT BREAK UP, REGEX NO ALLOW
-    regexp = re.compile('([!#-\'*+/-9=?A-Z^-~-]+(\.[!#-\'*+/-9=?A-Z^-~-]+)*|"([]!#-[^-~ \t]|(\\[\t -~]))+")@([!#-\'*+/\-9=?A-Z^-~-]+(\.[!#-\'*+/-9=?A-Z^-~-]+)*|\[[\t -Z^-~]*])')
+    regexp = re.compile(r'([!#-\'*+/-9=?A-Z^-~-]+(\.[!#-\'*+/-9=?A-Z^-~-]+)*|"([]!#-[^-~ \t]|(\\[\t -~]))+")@([!#-\'*+/\-9=?A-Z^-~-]+(\.[!#-\'*+/-9=?A-Z^-~-]+)*|\[[\t -Z^-~]*])')
     if regexp.match(email) is None:
 
         return "Email"

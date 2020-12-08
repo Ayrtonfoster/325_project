603a75d423b34386139e7c8051d4557340d13421
TankyFranky <43577151+TankyFranky@users.noreply.github.com>
2020-12-08 15:43:29 -0500

    Added dynamic ID's for ticket entries in tables
    Added dynamic ID's for ticket entries in tables. Also started test_sell.py

--- CI-Python/qa327_test/frontend/test_sell.py
+++ CI-Python/qa327_test/frontend/test_sell.py
@@ -3,6 +3,7 @@ from seleniumbase import BaseCase
 
 from qa327_test.conftest import base_url
 from unittest.mock import patch
+from datetime import date, timedelta
 from qa327.models import db, User
 from werkzeug.security import generate_password_hash, check_password_hash
 
@@ -14,55 +15,136 @@ The tests will only test the frontend portion of the program by patching the bac
 the correct data base values.
 """
 
+dateTime = date.today()
+dateTime + timedelta(days=10)
+future_date = dateTime.strftime("%Y\t%m%d")
+format_date = dateTime.strftime("%Y-%m-%d")
+
 test_user = User(
-    email='test_frontend@test.com',
-    name='test_frontend',
-    password=generate_password_hash('test_frontend')
+    email='test_sellerpage@test.com',
+    name='test_sellerpage',
+    password=generate_password_hash('test_sellerPage!2')
 )
 
 # Moch some sample tickets
 test_tickets = [
-    {'name': 't1', 'price': '100'}
+    {'ticket_name': 'testTicket',
+     'ticket_price': '15', 'num_tickets': '15',
+     'ticket_date': format_date, 'ticket_owner': 'test_sellerpage@test.com'}
 ]
 
-class FrontEndSellFunctionTest(BaseCase):
+'''
+login_pass is a decorator which logs in the user before each test
+It contains tests to confirm that login was successful as well
+'''
 
-    # def test_sell_page_exists(self, *_):
-    #     # R4.0.1
-    #     # This requirement is deprecated, the /sell page is incorporated into the / page
-    #     pass
 
+@patch('qa327.backend.get_user', return_value=test_user)
+@patch('qa327.backend.get_all_tickets', return_value=test_tickets)
+@patch('qa327.backend.login_user', return_value=test_user)
+def login_pass(inner_function, self, *_):
     @patch('qa327.backend.get_user', return_value=test_user)
     @patch('qa327.backend.get_all_tickets', return_value=test_tickets)
     @patch('qa327.backend.login_user', return_value=test_user)
+    def wrapped_inner(self, *_):
+        self.open(base_url + '/login')
+        # fill email and password
+        self.type("#email", "test_sellerpage@test.com")
+        self.type("#password", "test_sellerPage!2")
+        # click enter button
+        self.click('input[type="submit"]')
+
+        cur_url = self.get_current_url()
+        self.assertEqual(cur_url, "http://localhost:8081/")
+        return inner_function(self, *_)
+
+    # return the wrapped version of the inner_function:
+    wrapped_inner.__name__ = inner_function.__name__
+    return wrapped_inner
+
+class FrontEndSellFunctionTest(BaseCase):
+
+    @login_pass
     def test_sell_name_regex(self, *_):
+
         # R4.1.1
-        # self.open(base_url + '/') # Go to page with ticket sale ability
-        #
-        # # Fill in ticket information
-        # self.type("#ticket_name", "  weinyHut")
-        # self.type("#num_tickets", "15")
-        # self.type("#ticket_price", "15")
-        # self.type("#ticket_date", "2021-06-23")
-        #
-        # # Hit 'Post Ticket'
-        # self.click('input[id="btn-submit"]')
-        #
-        # # Check for failed to sell ticket message
-        # self.assert_element("#sell_message")
-        # self.assert_text("Ticket name does not follow guideline", "#sell_message")
+        self.open(base_url) # Go to page with ticket sale ability
 
-        pass
+        # Fill in ticket information
+        self.type("#sell_ticket_name", "   spaceTest") # test with space as first entry
+        self.type("#sell_num_tickets", "15")
+        self.type("#sell_ticket_price", "15")
+        self.type("#sell_ticket_date", future_date) # use date that is ahead of todays date
+
+        # Hit 'Post Ticket'
+        self.click('input[id="sell_btn-submit"]')
+
+        # Check for failed to sell ticket message
+        self.assert_element("#sell_message")
+        self.assert_text("Ticket name does not follow guideline", "#sell_message")
+
+        self.open(base_url)  # Go to page with ticket sale ability
+
+        # Fill in ticket information
+        self.type("#sell_ticket_name", "@$$ @$$")  # non alphanumeric name
+        self.type("#sell_num_tickets", "15")
+        self.type("#sell_ticket_price", "15")
+        self.type("#sell_ticket_date", future_date)  # use date that is ahead of todays date
+
+        # Hit 'Post Ticket'
+        self.click('input[id="sell_btn-submit"]')
+
+        # Check for failed to sell ticket message
+        self.assert_element("#sell_message")
+        self.assert_text("Ticket name does not follow guideline", "#sell_message")
+
+    @login_pass
+    @patch('qa327.backend.get_all_tickets', return_value=test_tickets)
+    def test_sell_name_length(self, *_):
+        # R4.2.1
+
+        self.open(base_url)  # Go to page with ticket sale ability
+
+        # Fill in ticket information
+        self.type("#sell_ticket_name", "testTicket")
+        # test name shorter than 60 characters
+        self.type("#sell_num_tickets", "15")
+        self.type("#sell_ticket_price", "15")
+        self.type("#sell_ticket_date", future_date)  # use date that is ahead of todays date
+
+        # Hit 'Post Ticket'
+        self.click('input[id="sell_btn-submit"]')
+
+        # Check for posted ticket in table and /sell was reached
+        ticket_id = self.driver.find_element_by_id("testTicket%15%15%"
+                            + format_date + "%test_sellerpage@test.com").text
+
+        self.assertIn("testTicket", ticket_id)
+        cur_url = self.get_current_url()
+        self.assertEqual(cur_url, base_url + "/sell")
+
+    @login_pass
+    def test_sell_name_length_long(self, *_):
+        # R4.2.2
+
+        self.open(base_url)  # Go to page with ticket sale ability
+
+        # Fill in ticket information
+        self.type("#sell_ticket_name", "This Ticket Name Will Be Longer Than 60,"
+                                       " Characters and Will Fail for that Reason")
+                                            # test name longer than 60 characters
+        self.type("#sell_num_tickets", "15")
+        self.type("#sell_ticket_price", "15")
+        self.type("#sell_ticket_date", future_date)  # use date that is ahead of todays date
+
+        # Hit 'Post Ticket'
+        self.click('input[id="sell_btn-submit"]')
+
+        # Check for failed to sell ticket message
+        self.assert_element("#sell_message")
+        self.assert_text("Ticket name does not follow guideline", "#sell_message")
 
 
-    # def test_sell_name_length(self, *_):
-    #     # R4.2.1
-    #     pass
-    #
-    # def test_sell_name_length_long(self, *_):
-    #     # R4.2.2
-    #     pass
-    #
     # def test_sell_qunatity_inside(self, *_):
     #     # R4.3.1
     #     pass
